79f952093bc89f7048170a8c8de90693475c2e61
Lubern5 <99304345+Lubern5@users.noreply.github.com>
2023-12-30 10:48:19 -0500

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "cloud57" # Replace with your actual S3 bucket name
+    bucket = "lu.123" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
